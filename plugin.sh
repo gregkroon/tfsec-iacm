66792bb027008a7adaf992e1c5985c46104c22f2
urischeiner <59268818+urischeiner@users.noreply.github.com>
2023-05-03 13:27:37 -0700

    Update plugin.sh

--- plugin.sh
+++ plugin.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+echo 'Starting installing script'
 curl -s https://raw.githubusercontent.com/aquasecurity/tfsec/master/scripts/install_linux.sh | bash
+echo 'Script installed'
 tfsec --no-color --soft-fail ${PLUGIN_FLAGS}
